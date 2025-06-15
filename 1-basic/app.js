26a6c94b9bc34d709b491f0ff243313f3366d030
mmsstudent2021 <mmsstudent2021@gmail.com>
2025-06-15 13:38:23 +0700

    day 1 finish

---
+++ 1-basic/app.js
@@ -0,0 +1,93 @@
+
+
+
+// let x; // declare variable x
+// let y;
+
+// x = 5; // value assign to variable
+// y = 6;
+
+
+// console.log(x); // access variable
+// console.log(y);
+
+// console.log(x + y)
+// console.log(x * y)
+
+
+
+// let x = 5;
+// let y = 2 + x; // 7
+
+// console.log(y)
+
+// let money = 5;
+
+// console.log(money);
+
+// money = 7;
+
+// console.log(money);
+
+// money = 4;
+
+// console.log(money)
+
+
+// HTML Version 5
+// CSS Version 3
+// Tailwind Version 4
+// JavaScript Version ES6
+
+
+// const ?
+
+// const daysInAWeek = 7;
+
+// console.log(daysInAWeek)
+
+// daysInAWeek = 10;
+
+// console.log(daysInAWeek)
+
+
+// Naming Convention
+
+// hein htet zan - heinHtetZan - HeinHtetZan - hein-htet-zan - hein_htet_zan
+ 
+// I love you - iLoveYou - ILoveYou - i-love-you - i_love_you
+
+// let MyName = "hein htet zan";
+// let myname = "hein htet zan";
+// console.log(MyName)
+// console.log(myname)
+
+// let x1 = 5;
+// let x2 = 10;
+// let $x = 15;
+// let _x = 15;
+
+
+// es 5
+// let myName = "hein htet zan";
+// let myWeight = '101kg'
+// // let myAge = 18;
+// let birthYear = 1994;
+
+
+
+// // es 6
+// let intro = `My name is ${myName} and I'm ${ 2025 - birthYear } years old`;
+
+// console.log(intro)
+
+// console.log("My phone screen is 6\" and OLED ")
+// console.log('My phone screen is 6" and OLED ')
+// console.log('My ipad screen is 12\' 9" and OLED ')
+// console.log(`My phone screen is 6" and OLED`)
+
+
+// let myAge = 31;
+// let howTallIAm = 132;
+// let weight = 101.5;
+// let roomTem = -10;
