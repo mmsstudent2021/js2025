023c801f31bf5050964b94e4eb385cf9b4c2bc6b
mmsstudent2021 <mmsstudent2021@gmail.com>
2025-06-17 19:01:29 +0700

    day 4 types

--- 1-basic/app.js
+++ 1-basic/app.js
@@ -54,7 +54,7 @@
 // Naming Convention
 
 // hein htet zan - heinHtetZan - HeinHtetZan - hein-htet-zan - hein_htet_zan
- 
+
 // I love you - iLoveYou - ILoveYou - i-love-you - i_love_you
 
 // let MyName = "hein htet zan";
@@ -91,3 +91,226 @@
 // let howTallIAm = 132;
 // let weight = 101.5;
 // let roomTem = -10;
+
+// console.log(typeof "hhz")
+// console.log(typeof 'hello')
+// console.log(typeof `abcd`)
+// console.log(typeof 123)
+// console.log(typeof -10)
+// console.log(typeof 101.5)
+
+
+// let x = null;
+
+// console.log(x)
+
+// let y;
+
+// console.log(y)
+
+// let z;
+
+// console.log(z)
+
+
+// let lunchBox = ["rice","curry","soup","fruit"];
+
+// // console.log(typeof arr)
+
+// console.log(lunchBox)
+// console.log(lunchBox[0])
+// console.log(lunchBox[1])
+
+
+// const marks = [50,60,65,80,80,55];
+// console.log(marks)
+
+// marks = 450;
+
+// console.log(marks[2])
+
+// marks[2] = 75;
+
+// console.log(marks)
+
+// let myself = ["hein htet zan",18,"Bangkok","Tech Lead", false]
+
+// let fruits = [];
+
+// fruits[0] = "apple"; // append
+// fruits[1] = "orange";
+// fruits[2] = "mango"
+// fruits[3] = "lemon";
+// // fruits[7] = "lime";
+
+// console.log(fruits)
+// console.log(fruits.length)
+
+// fruits[1] = "lemon" // overwrite
+
+// console.log(fruits)
+
+
+// let myself = {
+//     myName: "hein htet zan",
+//     myAge: 18,
+//     job: "Teach Lead",
+//     location : "Bangkok",
+//     girlFriend: null,
+//     weight: 101.5,
+//     weight: "kg",
+//     skills: ["web development", "teaching", "negotiation"]
+// }
+
+// console.log(myself)
+// console.log(myself["myName"])
+// console.log(myself.myName)
+// console.log(`My name is ${myself.myName} and ${myself.myAge} years old, currently live in ${myself.location}. Work as ${myself.job}`)
+
+
+// let marks = {
+//     mm: 50,
+//     eng: 65,
+//     math: 80,
+//     phy: 60,
+//     che: 80,
+//     bio: 55
+// }
+
+// console.log(marks)
+
+// marks.eng = 80;
+
+// console.log(marks)
+
+
+// let obj = {};
+
+// obj.a = "aaa";
+// obj.b = "bbb";
+// obj.c = "ccc"
+
+// console.log(obj);
+
+// obj.b = "xxx";
+
+// console.log(obj)
+
+
+// let arr = [];
+// console.log(arr);
+
+
+// const fruits = ["apple","orange","mango"];
+
+// fruits = "hello"
+
+// console.log(fruits)
+
+// fruits[1] = "banana";
+
+// console.log(fruits)
+// console.log(fruits[2])
+// console.log(fruits[5])
+
+// const exchangeRates = {
+//     usd : 4800,
+//     sgd : 3700,
+//     thb : 132
+// }
+
+// console.log(exchangeRates)
+
+// exchangeRates.sgd = 3900;
+
+// console.log(exchangeRates)
+// console.log(exchangeRates.usd)
+// console.log(exchangeRates.eur)
+
+// let x = 5;
+
+// let y = x;
+
+// console.log("x",x);
+// console.log("y",y);
+
+// x = 10;
+
+// console.log("x",x);
+// console.log("y",y);
+
+
+// const x = ["a","b","c"];
+
+// const y = x;
+
+// console.log("x",x);
+// console.log("y",y);
+
+// x[1] = "bbb";
+
+// console.log("x",x);
+// console.log("y",y);
+
+const name = "Mg Mg";
+const age = 15;
+const township = "kyauk myaung";
+let money = 200;
+const orangePrice = 50;
+money = money - orangePrice;
+console.log(money);
+
+const bag = ["mm", "eng", "bio"];
+const pen = null;
+const canSmokeMgMg = false;
+const canSmokeMgMgFriend = true;
+
+const student1 = {
+    name,
+    age,
+    gender: "male",
+    money,
+    canSmokeMgMg,
+    bag
+}
+
+// console.log(mgmgInfo);
+// console.log(mgmgInfo.name);
+// console.log(mgmgInfo.bag[1]);
+
+const student2 = {
+    name: "Kyaw Kyaw",
+    age: 16,
+    gender: "male",
+}
+
+
+const student3 = {
+    name: "zaw zaw",
+    age: 15,
+    gender: "male",
+
+}
+
+const student4 = {
+    name: "mya mya",
+    age: 16,
+    gender: "female",
+}
+
+const students = [student1,student2,student3,student4];
+
+console.log(students);
+console.log(students[1].age);
+console.log(students[1]["age"]);
+console.log(students[0]["bag"][2]);
+console.log(students[0].bag[2]);
+
+
+
+
+
+
+
+
+
