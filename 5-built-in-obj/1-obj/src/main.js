cc3760614bdcb2ac2424ae882243edd17ed84981
mmsstudent2021 <mmsstudent2021@gmail.com>
2025-07-02 20:50:57 +0700

    day 10 part 1

---
+++ 5-built-in-obj/1-obj/src/main.js
@@ -0,0 +1,84 @@
+import dayjs from "dayjs";
+
+// const dayjs = require("dayjs");
+
+// const date = new Date();
+
+// console.log(date);
+
+// const now = dayjs();
+// console.log(now.add(150,"day").format("DD-MM-YYYY"));
+// console.log(now.format("DD-MM-YYYY"));
+// console.log(now.format('HH:mm:ss A'));
+
+// const currentDate = date.getDate();
+// date.setDate(currentDate + 250)
+// const currentMonth = date.getMonth();
+// date.setMonth(currentMonth + 4);
+// console.log(date);
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toISOString());
+// console.log(date.toUTCString());
+// console.dir(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getTime());
+
+// const monthNames = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// console.log(
+//   `${date.getFullYear()} - ${monthNames[date.getMonth()]} - ${date.getDate()}`
+// );
+
+// const now = dayjs()
+
+// 22 7 1994
+
+// const date = new Date("1994-07-22");
+// const date = new Date("1994-07-22T04:55:00");
+// const date = new Date(1994,7,22);
+
+// console.log(date);
+
+// const examDate = "2026-01-23";
+// const examDateObj = new Date(examDate);
+// console.log(examDateObj);
+
+// const currentDateObj = new Date();
+
+// const resultInMillisecond = examDateObj - currentDateObj;
+
+// // Convert milliseconds to days
+// const msInOneDay = 1000 * 60 * 60 * 24;
+// const dayDifference = resultInMillisecond / msInOneDay;
+
+// console.log(dayDifference);
+
+// const examDate = dayjs("2026-01-23");
+// const today = dayjs();
+
+// console.log(examDate.diff(today,"month"));
+
+console.log(Date.now());
